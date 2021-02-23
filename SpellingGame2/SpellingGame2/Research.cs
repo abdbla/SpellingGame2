90e897ff64792ddde727e1d27573663f299a2049
abdbla <felixmurnion@gmail.com>
2021-02-23 21:33:50 +0100

    day one of not being lazy
    so like, objects may need to access the engine later, and if they do, its going to be a bitch to add that. So im adding the engine into the research delegates, just in case. May turn out to be a non-necessity, but i can imagine cases where it would happen, assuming the design doesn't change later.

--- SpellingGame2/SpellingGame2/Research.cs
+++ SpellingGame2/SpellingGame2/Research.cs
@@ -7,9 +7,9 @@ namespace SpellingGame2
 {
     static class Research
     {
-        static Dictionary<ObjectID, Action<IUserInterface, Player>> GetResearch() {
-            Dictionary<ObjectID, Action<IUserInterface, Player>> research = new Dictionary<ObjectID, Action<IUserInterface, Player>>();
-            research.Add(ObjectID.UnceasingTop, delegate (IUserInterface ui, Player p)
+        static Dictionary<ObjectID, Action<IUserInterface, Player, Engine>> GetResearch() {
+            Dictionary<ObjectID, Action<IUserInterface, Player, Engine>> research = new Dictionary<ObjectID, Action<IUserInterface, Player, Engine>>();
+            research.Add(ObjectID.UnceasingTop, delegate (IUserInterface ui, Player p, Engine e)
             {
 
             });
